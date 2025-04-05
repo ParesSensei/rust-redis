7721814a6b10b2c3d588e1244a8f59ab8470e356
Pares <alfariz2004@gmail.com>
2025-04-06 04:20:56 +0700

    create redis client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use redis::{AsyncCommands, Client, Commands, RedisError};
+    use redis::aio::MultiplexedConnection;
+
+    #[tokio::test]
+    async fn test_async_connection() -> Result<(), RedisError> {
+        let mut con = get_client().await?;
+        let _ : () = con.set("name", "kurniawan").await?;
+        let value: String = con.get("name").await?;
+
+        println!("value: {}", value);
+        Ok(())
+    }
+
+    async fn get_client() -> Result<MultiplexedConnection, RedisError> {
+        let client = Client::open("redis://localhost:6379/")?;
+        client.get_multiplexed_async_connection().await
+    }
+
+    #[test]
+    fn test_connection() {
+        let mut client = Client::open("redis://localhost:6379/").unwrap();
+
+        let _: () = client.set("name", "Pares").unwrap();
+        let value: String = client.get("name").unwrap();
+
+        println!("{}", value)
+    }
+}
